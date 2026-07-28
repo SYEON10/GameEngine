a8e089cd7745af31e013eec5b406b3674785b0e3
syeon10 <syeon.dev@gmail.com>
2026-07-29 00:11:42 +0900

    PCH 추가

---
+++ WBEngine/WBEngine/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// ===== 1. 표준 라이브러리: 컨테이너 =====
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+// ===== 2. 표준 라이브러리: 유틸 =====
+#include <memory>       // unique_ptr, shared_ptr
+#include <functional>   // std::function, delegate 용
+#include <algorithm>
+#include <optional>
+
+// ===== 3. 표준 라이브러리: 수학/IO =====
+#include <cmath>
+#include <cstdint> 
